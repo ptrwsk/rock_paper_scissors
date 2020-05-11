41c6a4472f93144dac8edb14d01d94e734dbf775
Zuzia Piotrowska <zuzannapiotrowska2002@gmail.com>
2020-05-12 01:07:56 +0200

    Now it works- does one round

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-function computerPlay () {
+function computerPlay() {
     let max = 3
     let min = 1
-    function randomWholeNumber () {
+    function randomWholeNumber() {
        return Math.floor(Math.random() * (max - min + 1)) + min
     }
-    switch (randomWholeNumber) {
+    switch (randomWholeNumber()) {
         case 1:
             return "rock";
-            break;
         case 2:
             return "paper";
-            break;
         case 3:
             return "scissors";
-            break;
 
     }
     
 }
 
-let playerSelection = (window.prompt("Choose rock, paper or scissors :)", ""))
+let playerSelection = window.prompt("Choose rock, paper or scissors :)", "")
+
+const computerSelection = computerPlay()
 
 function singleRound (playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == "rock") {
-        if (computerSelection = "rock") {
+    let newPlayerSelection = playerSelection.toLowerCase();
+
+    if (newPlayerSelection == "rock") {
+        if (computerSelection == "rock") {
             return "It's a draw!"
         }
-        else if (computerSelection = "paper") {
+        else if (computerSelection == "paper") {
             return "Oopsie! You lost ;((("
         }
         else {
             return "YEAAH YOU WON!"
         }
     }
-    if (playerSelection.toLowerCase() == "paper") {
-        if (computerSelection = "paper") {
+    if (newPlayerSelection == "paper") {
+        if (computerSelection == "paper") {
             return "It's a draw!"
         }
-        else if (computerSelection = "scissors") {
+        else if (computerSelection == "scissors") {
             return "Oopsie! You lost ;((("
         }
         else {
             return "YEAAH YOU WON!"
         }
     }
-    if (playerSelection.toLowerCase() == "scissors") {
-        if (computerSelection = "scissors") {
+    if (newPlayerSelection == "scissors") {
+        if (computerSelection == "scissors") {
             return "It's a draw!"
         }
-        else if (computerSelection = "rock") {
+        else if (computerSelection == "rock") {
             return "Oopsie! You lost ;((("
         }
         else {
@@ -58,3 +59,8 @@ function singleRound (playerSelection, computerSelection) {
 
     
 }
+
+console.log(singleRound (playerSelection, computerSelection))
+
+//function game()
+
